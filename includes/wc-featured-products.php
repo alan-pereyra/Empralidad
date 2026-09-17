66b44e4233498efc79549843d0af96eaa36a4eed
alan-pereyra <alanpereyra57@gmail.com>
2026-09-17 16:33:54 -0300

    fix(woocommerce): version 9.4.8, remover fondo invertido en destacados y ajustar regla responsiva

--- includes/wc-featured-products.php
+++ includes/wc-featured-products.php
@@ -2,7 +2,7 @@
 <?php $featured_products_margin_negative = get_theme_mod('emp_woocommerce_featured_margin_negative'); ?>
 <?php $featured_products_h2 = get_theme_mod('emp_woocommerce_featured_title'); ?>
 <?php if($featured_products && class_exists('WooCommerce')) { ?>
-  <div class="color-content-invert bg-content-invert pb-5 <?php if($featured_products_margin_negative){ ?> mt-250-n <?php } ?>">
+  <div class="color-content-invert pb-5 <?php if($featured_products_margin_negative){ ?> mt-250-n <?php } ?>">
     <?php if($featured_products_h2){ ?>
       <div class="<?php if($featured_products_margin_negative){ ?> title-color <?php } ?>">
         <h2 class="featured-title"><?php echo get_theme_mod('emp_woocommerce_featured_title'); ?></h2>
