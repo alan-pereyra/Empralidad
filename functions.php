1d59b9a0fbacc92c27c6811ed911d1ff1e78bf32
alan-pereyra <alanpereyra57@gmail.com>
2026-09-17 14:11:20 -0300

    feat(woocommerce): redisenar tarjeta de producto en loop con boton circular adaptable y precio centrado

--- functions.php
+++ functions.php
@@ -288,6 +288,22 @@ if (class_exists('WooCommerce')){
     return $fragments;
   }
   add_filter( 'woocommerce_add_to_cart_fragments', 'wc_refresh_mini_cart_count');
+
+  // Circular add to cart button with plus icon for loop
+  function emp_loop_add_to_cart_button( $html, $product, $args ) {
+    $icon = '<i class="fa fa-plus"></i>';
+    return sprintf(
+      '<a href="%s" data-quantity="%s" class="%s emp-loop-btn-circle" %s title="%s" aria-label="%s">%s</a>',
+      esc_url( $product->add_to_cart_url() ),
+      esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+      esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+      isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+      esc_attr( $product->add_to_cart_text() ),
+      esc_attr( $product->add_to_cart_text() ),
+      $icon
+    );
+  }
+  add_filter( 'woocommerce_loop_add_to_cart_link', 'emp_loop_add_to_cart_button', 10, 3 );
 }
 
 // Add custom field on woocommerce page
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) || exit;
 
 global $product;
 
-$id = $product->id;
+$id = $product ? $product->get_id() : get_the_ID();
 $icon_url = esc_url( get_template_directory_uri() ).'/includes/icons/';
 
 // Ensure visibility.
@@ -27,7 +27,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php wc_product_class( '', $product ); ?>>
+<li <?php wc_product_class( 'emp-product-card', $product ); ?>>
 	<?php
 	/**
 	 * Hook: woocommerce_before_shop_loop_item.
@@ -52,75 +52,50 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	do_action( 'woocommerce_shop_loop_item_title' );
 
 	/**
-	 * Hook: woocommerce_after_shop_loop_item_title.
-	 *
-	 * @hooked woocommerce_template_loop_rating - 5
-	 * @hooked woocommerce_template_loop_price - 10
+	 * Close product link right after title so the action button below isn't nested inside <a>
 	 */
-	do_action( 'woocommerce_after_shop_loop_item_title' );
-
-    // Add custom field
-    $display = '';
-    if(get_post_meta($id, '_custom_product_component_field_cpu', true)){
-        $display = 'd-flex';
-    }
-    echo '<div class="woocommerce-custom-field-pc-gaming-loop '.$display.'">';
-        if(get_post_meta($id, '_custom_product_component_field_cpu', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'cpu.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_cpu', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_mother', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'mother.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_mother', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_gpu', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'gpu.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_gpu', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_ram', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'ram.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_ram', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_ssd_hdd', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'ssd.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_ssd_hdd', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_power_supply', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'power_supply.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_power_supply', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_chassis', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'chassis.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_chassis', true);
-            echo '</div>';
-        }
-        if(get_post_meta($id, '_custom_product_component_field_display', true)){
-            echo '<div class="wrap">
-                    <img src="'.$icon_url.'display.svg" />';
-            echo    get_post_meta($id, '_custom_product_component_field_display', true);
-            echo '</div>';
-        }
-    echo '</div>';
-
-	/**
-	 * Hook: woocommerce_after_shop_loop_item.
-	 *
-	 * @hooked woocommerce_template_loop_product_link_close - 5
-	 * @hooked woocommerce_template_loop_add_to_cart - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop_item' );
+	woocommerce_template_loop_product_link_close();
 	?>
+
+	<?php
+    // Add custom field (PC Gaming icons loop)
+    $has_cpu = get_post_meta($id, '_custom_product_component_field_cpu', true);
+    if( $has_cpu ){
+        echo '<div class="woocommerce-custom-field-pc-gaming-loop d-flex">';
+        if($has_cpu){
+            echo '<div class="wrap"><img src="'.$icon_url.'cpu.svg" />'.esc_html($has_cpu).'</div>';
+        }
+        if($mother = get_post_meta($id, '_custom_product_component_field_mother', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'mother.svg" />'.esc_html($mother).'</div>';
+        }
+        if($gpu = get_post_meta($id, '_custom_product_component_field_gpu', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'gpu.svg" />'.esc_html($gpu).'</div>';
+        }
+        if($ram = get_post_meta($id, '_custom_product_component_field_ram', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'ram.svg" />'.esc_html($ram).'</div>';
+        }
+        if($ssd = get_post_meta($id, '_custom_product_component_field_ssd_hdd', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'ssd.svg" />'.esc_html($ssd).'</div>';
+        }
+        if($psu = get_post_meta($id, '_custom_product_component_field_power_supply', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'power_supply.svg" />'.esc_html($psu).'</div>';
+        }
+        if($chassis = get_post_meta($id, '_custom_product_component_field_chassis', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'chassis.svg" />'.esc_html($chassis).'</div>';
+        }
+        if($display = get_post_meta($id, '_custom_product_component_field_display', true)){
+            echo '<div class="wrap"><img src="'.$icon_url.'display.svg" />'.esc_html($display).'</div>';
+        }
+        echo '</div>';
+    }
+    ?>
+
+	<div class="emp-product-bottom-row">
+		<div class="emp-product-price-centered">
+			<?php woocommerce_template_loop_price(); ?>
+		</div>
+		<div class="emp-product-action-right">
+			<?php woocommerce_template_loop_add_to_cart(); ?>
+		</div>
+	</div>
 </li>
