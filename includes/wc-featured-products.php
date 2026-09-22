6f42566378f8c9a0ea6e0c9d9c96cb160a714caf
alan-pereyra <alanpereyra57@gmail.com>
2026-09-22 12:35:27 -0300

    style(homepage): reduce bottom spacing of featured products title to bring it closer to product cards

--- includes/wc-featured-products.php
+++ includes/wc-featured-products.php
@@ -1,15 +1,18 @@
 <?php $featured_products = get_theme_mod('emp_woocommerce_featured_show'); ?>
 <?php $featured_products_margin_negative = get_theme_mod('emp_woocommerce_featured_margin_negative'); ?>
 <?php $featured_products_h2 = get_theme_mod('emp_woocommerce_featured_title'); ?>
+<?php $featured_products_text = get_theme_mod('emp_woocommerce_featured_text'); ?>
 <?php if($featured_products && class_exists('WooCommerce')) { ?>
   <div class="pb-5 <?php if($featured_products_margin_negative){ ?> mt-250-n <?php } ?>">
     <?php if($featured_products_h2){ ?>
       <div class="">
         <h2 class="featured-title featured-title-invert"><?php echo get_theme_mod('emp_woocommerce_featured_title'); ?></h2>
-        <p class="py-2 mt-n5 text-center"><small><?php echo get_theme_mod('emp_woocommerce_featured_text'); ?></small></p>
+        <?php if($featured_products_text){ ?>
+          <p class="py-2 mt-n5 text-center"><small><?php echo esc_html($featured_products_text); ?></small></p>
+        <?php } ?>
       </div>
     <?php } ?>
-    <div id="woo_featured_products" class="px-1 py-4 col-12 mx-auto text-center overflow-hidden">
+    <div id="woo_featured_products" class="px-1 pt-0 pb-4 col-12 mx-auto text-center overflow-hidden">
       <?php echo do_shortcode('[featured_products columns="3" orderby="price" order="ASC"]'); ?>
     </div>
   </div>
